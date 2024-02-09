16d1f7d02123f82553a0b95c3ed30947b1bed6c7
Nishant Nikam <nikamn@gmx.com>
2024-02-09 11:41:15 +0000

    modified docs

---
+++ docs/db/sql_commands/categories.sql
@@ -0,0 +1,7 @@
+INSERT INTO categories (id, category_name, description) VALUES (1, 'Seeds', 'descrption 1');
+INSERT INTO categories (id, category_name, description) VALUES (2, 'Pesticides', 'descrption 2');
+INSERT INTO categories (id, category_name, description) VALUES (3,  'Fertilizers', 'descrption 3');
+INSERT INTO categories (id, category_name, description) VALUES (4, 'Grow Bags', 'descrption 4');
+INSERT INTO categories (id, category_name, description) VALUES (5, 'Plants', 'descrption 5');
+INSERT INTO categories (id, category_name, description) VALUES (6, 'Manures', 'descrption 6');
+INSERT INTO categories (id, category_name, description) VALUES (7,'Gardening Equipments', 'descrption 7');
--- docs/db/sql_commands/category.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO categories (category_id, category_name) VALUES (1, 'Seeds');
-INSERT INTO categories (category_id, category_name) VALUES (2, 'Pesticides');
-INSERT INTO categories (category_id, category_name) VALUES (3,  'Fertilizers');
-INSERT INTO categories (category_id, category_name) VALUES (4, 'Grow Bags');
-INSERT INTO categories (category_id, category_name) VALUES (5, 'Plants');
-INSERT INTO categories (category_id, category_name) VALUES (6, 'Manures');
-INSERT INTO categories (category_id, category_name) VALUES (7,'Gardening Equipments');
