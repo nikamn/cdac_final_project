df75c619a02d96e633b197d71ed889c45dc1cef8
Nishant Nikam <nikamn@gmx.com>
2024-02-12 15:57:14 +0000

    added db docs

--- docs/db/sql_commands/users.sql
+++ docs/db/sql_commands/users.sql
@@ -1,10 +1,11 @@
-INSERT INTO users (id, first_name, last_name, email, password, mobile_no) VALUES (1, 'John', 'Doe', 'johndoe@example.com', 'password123', '1234567890');
-INSERT INTO users (id, first_name, last_name, email, password, mobile_no) VALUES (2, 'Jane', 'Smith', 'janesmith@example.com', 'jane123', '9999999999');
-INSERT INTO users (id, first_name, last_name, email, password, mobile_no) VALUES (3, 'Michael', 'Johnson', 'michaelj@example.com', 'securepassword', '6776776767');
-INSERT INTO users (id, first_name, last_name, email, password, mobile_no) VALUES (4, 'Emily', 'Brown', 'emilyb@example.com', 'mypassword', '7777777777');
-INSERT INTO users (id, first_name, last_name, email, password, mobile_no) VALUES (5, 'David', 'Wilson', 'davidw@example.com', 'password123', '9999999999');
-INSERT INTO users (id, first_name, last_name, email, password, mobile_no) VALUES (6, 'Sarah', 'Taylor', 'saraht@example.com', 'securepassword', '1111111111');
-INSERT INTO users (id, first_name, last_name, email, password, mobile_no) VALUES (7, 'Robert', 'Anderson', 'roberta@example.com', 'mypassword', '2222222222');
-INSERT INTO users (id, first_name, last_name, email, password, mobile_no) VALUES (8, 'Jessica', 'Lee', 'jessical@example.com', 'password123', '3333333333');
-INSERT INTO users (id, first_name, last_name, email, password, mobile_no) VALUES (9, 'Christopher', 'Clark', 'christopherc@example.com', 'securepassword', '4444444444');
-INSERT INTO users (id, first_name, last_name, email, password, mobile_no) VALUES (10, 'Amanda', 'Moore', 'amandam@example.com', 'mypassword', '6666666666');
+INSERT INTO users (id, first_name, last_name, username, email, password, mobile_no) VALUES
+(1, 'John', 'Doe', 'johndoe', 'johndoe@example.com', 'password123', '1234567890'),
+(2, 'Jane', 'Smith', 'janesmith', 'janesmith@example.com', 'jane123', '9999999999'),
+(3, 'Michael', 'Johnson', 'michaelj', 'michaelj@example.com', 'securepassword', '6776776767'),
+(4, 'Emily', 'Brown', 'emilyb', 'emilyb@example.com', 'mypassword', '7777777777'),
+(5, 'David', 'Wilson', 'davidw', 'davidw@example.com', 'password123', '9999999999'),
+(6, 'Sarah', 'Taylor', 'saraht', 'saraht@example.com', 'securepassword', '1111111111'),
+(7, 'Robert', 'Anderson', 'roberta', 'roberta@example.com', 'mypassword', '2222222222'),
+(8, 'Jessica', 'Lee', 'jessical', 'jessical@example.com', 'password123', '3333333333'),
+(9, 'Christopher', 'Clark', 'christopherc', 'christopherc@example.com', 'securepassword', '4444444444'),
+(10, 'Amanda', 'Moore', 'amandam', 'amandam@example.com', 'mypassword', '6666666666');
