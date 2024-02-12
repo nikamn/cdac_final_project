1d2a9cc9547a5d638f6f3d9c914c49b6ea86500b
patidar-arpit <arpitpaidar810985@gmail.com>
2024-02-13 00:19:24 +0530

    some changes in wishlist controller

--- backend/java/roottoroof/src/main/java/com/acts/controller/WishListController.java
+++ backend/java/roottoroof/src/main/java/com/acts/controller/WishListController.java
@@ -4,10 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.acts.dto.ApiResponse;
@@ -27,8 +27,8 @@ public class WishListController {
          @Autowired
         private AuthenticationService authenticationService;
 
-        @PostMapping("/add/{token}")
-        public ResponseEntity<ApiResponse> addWishList(@RequestBody Product product,@PathVariable String token) {
+        @PostMapping("/add")
+        public ResponseEntity<ApiResponse> addWishList(@RequestBody Product product,@RequestParam("token") String token) {
                 authenticationService.authenticate(token);
                 User user = authenticationService.getUser(token);
                 WishList wishList = new WishList(user, product);
@@ -37,8 +37,8 @@ public class WishListController {
 
         }
 
-        @GetMapping("/{token}")
-        public ResponseEntity<?> getWishList(@PathVariable String token) {
+        @GetMapping("/")
+        public ResponseEntity<?> getWishList(@RequestParam("token") String token) {
                 
                 return  ResponseEntity.ok(wishListService.getWishList(token));
         }
