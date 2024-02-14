32bed8ca003a856ed1ffaefe1eee9e2779f4367d
anki009 <ankum1999@gmail.com>
2024-02-14 13:43:56 +0530

    fixed typo

--- backend/java/roottoroof/src/main/java/com/acts/service/impl/UserServiceImpl.java
+++ backend/java/roottoroof/src/main/java/com/acts/service/impl/UserServiceImpl.java
@@ -176,7 +176,7 @@ public class UserServiceImpl implements UserService {
             throw new CustomException("token is not Present");
         }
         UserDTO userDetails = mapper.map(user.get(), UserDTO.class);
-        return new SigninResponse(userDetails, "sucess", token.getToken());
+        return new SigninResponse(userDetails, "success", token.getToken());
 
     }
 }
