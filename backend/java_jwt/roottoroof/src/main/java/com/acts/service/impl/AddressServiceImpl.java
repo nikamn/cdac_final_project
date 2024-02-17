6070380b4845055094b0b8b2c4ff64647a8f6971
anki009 <ankum1999@gmail.com>
2024-02-17 19:24:30 +0530

    delete address functionality modified

--- backend/java_jwt/roottoroof/src/main/java/com/acts/service/impl/AddressServiceImpl.java
+++ backend/java_jwt/roottoroof/src/main/java/com/acts/service/impl/AddressServiceImpl.java
@@ -1,6 +1,5 @@
 package com.acts.service.impl;
 
-
 import javax.transaction.Transactional;
 
 import org.modelmapper.ModelMapper;
@@ -18,23 +17,21 @@ import com.acts.service.AddressService;
 
 import lombok.RequiredArgsConstructor;
 
-
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class AddressServiceImpl implements AddressService {
-	
+
 	private final UserRepository customerRepository;
 
-	
 	private final AddressRepository adrRepo;
 
 	@Autowired
-	private  ModelMapper mapper;	
+	private ModelMapper mapper;
 
 	@Override
 	public AddressDTO getAddressDetails(Integer addressId) {
-		
+
 		return mapper.map(
 				adrRepo.findById(addressId).orElseThrow(
 						() -> new ResourceNotFoundException("Invalid Emp  Id Or Address not yet assigned !!!!")),
@@ -49,54 +46,52 @@ public class AddressServiceImpl implements AddressService {
 		System.out.println(address.toString());
 
 		User customer = customerRepository.findById(customerId)
-		                 .orElseThrow(()-> new ResourceNotFoundException("invaid customer id"));
-        
+				.orElseThrow(() -> new ResourceNotFoundException("invaid customer id"));
 
 		System.out.println(customer.toString());
 
 		// map dto --> entity
 		Address addressEntity = mapper.map(address, Address.class);
-        
+
 		System.out.println(addressEntity.toString());
 
 		// establish un dir link , adr ---> emp
 		addressEntity.setOwner(customer);
 		// save adr details
 		System.out.println(addressEntity.toString());
-        adrRepo.save(addressEntity);
-		
-		return new ApiResponse("Assigned new Address to Customer,"+ customer.getFirstName());
-			
-    }
+		adrRepo.save(addressEntity);
+
+		return new ApiResponse("Assigned new Address to Customer," + customer.getFirstName());
+
+	}
 
 	@Override
 	public ApiResponse updateUserAddress(Integer empId, AddressDTO address) {
 
 		Address addressEntity = adrRepo.findById(empId)
-		                        .orElseThrow(()-> new ResourceNotFoundException("Address is not yet Assigned"));
-	
+				.orElseThrow(() -> new ResourceNotFoundException("Address is not yet Assigned"));
+
 		mapper.map(address, addressEntity);
 		// save adr details
 		adrRepo.save(addressEntity);
-		return new ApiResponse("Updated address for Emp" +addressEntity.getId()); 
-		
+		return new ApiResponse("Updated address for Emp" + addressEntity.getId());
+
 	}
 
-    @Override
-    public ApiResponse deleteUserAddress(Integer customerId) {
+	@Override
+	public ApiResponse deleteUserAddress(Integer customerId) {
 
-        Address address=adrRepo.findById(customerId).orElseThrow(()->new ResourceNotFoundException("Address not found"));
-		
+		Address address = adrRepo.findById(customerId)
+				.orElseThrow(() -> new ResourceNotFoundException("Address not found"));
+		adrRepo.delete(address);
 		return new ApiResponse("Address deleted of customer with id " + address.getId());
-			
 
 	}
 
 	// Function<Address, AddressDTO> getAddressDTOFromAdress = address -> {
-    //     AddressDTO addressDTO = mapper.map(address, AddressDTO.class);
-    //     //addressDTO.setId(address.getId());
-    //     return addressDTO;
-    // };
-
+	// AddressDTO addressDTO = mapper.map(address, AddressDTO.class);
+	// //addressDTO.setId(address.getId());
+	// return addressDTO;
+	// };
 
 }
