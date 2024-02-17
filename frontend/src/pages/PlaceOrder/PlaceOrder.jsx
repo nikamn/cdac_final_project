e16f21d73698c22a17b0b7deb4a036aa466c0b67
anki009 <ankum1999@gmail.com>
2024-02-18 00:23:20 +0530

    modified style placeorder page

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -2,16 +2,13 @@ import Address from "../../components/Address/Adress";
 import Cart from "../../components/Cart/Cart";
 
 const PlaceOrder = () => {
-  // const { showModal, toggle, cartItems, addToCart } = useContext(CartContext);
-
-  //   console.log(toggle);
   return (
-    <section className="grid grid-cols-1 md:grid-cols-2 ">
-      <div className="">
+    <section className="sm:mt-3  relative max-w-[1000px] grid grid-cols-1 md:grid-cols-2 mx-auto  rounded-md">
+      <div className="w-full h-auto p-5 flex justify-center items-center bg-[#1D2E28] md:rounded-l-md">
         <Address />
       </div>
 
-      <div className="w-1/2">
+      <div className="w-full bg-[#CBD5C0] flex flex-col justify-center items-center p-5 md:rounded-r-md">
         <Cart />
       </div>
     </section>
