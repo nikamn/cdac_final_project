1dafb842d6ff19c2a9c291d6c7d5c5dd9832eeab
anki009 <ankum1999@gmail.com>
2024-02-18 00:34:44 +0530

    modified style

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -3,7 +3,7 @@ import Cart from "../../components/Cart/Cart";
 
 const PlaceOrder = () => {
   return (
-    <section className="sm:mt-3  relative max-w-[1000px] grid grid-cols-1 md:grid-cols-2 mx-auto  rounded-md">
+    <section className="sm:mt-3  relative w-full max-w-[1100px] grid grid-cols-1 md:grid-cols-2 mx-auto  rounded-md">
       <div className="w-full h-auto p-5 flex justify-center items-center bg-[#1D2E28] md:rounded-l-md">
         <Address />
       </div>
