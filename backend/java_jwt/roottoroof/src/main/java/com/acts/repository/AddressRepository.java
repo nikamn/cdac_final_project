6c388001d5a6052015ecc1bb375c6ab26fc108d0
Arpit <arpitpatidar810985@gmail.com>
2024-02-19 00:02:53 +0530

    Added some Address and Category Repository Related Test cases

--- backend/java_jwt/roottoroof/src/main/java/com/acts/repository/AddressRepository.java
+++ backend/java_jwt/roottoroof/src/main/java/com/acts/repository/AddressRepository.java
@@ -1,9 +1,19 @@
 package com.acts.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.acts.model.Address;
+import com.acts.model.User;
 
 public interface AddressRepository extends JpaRepository<Address, Integer> {
+
+    long deleteAddressDetailsByState(String string);
+
+    
+    @Query("select a.owner from Address a where a.city=:city")
+    List<User> findUsersByCity(String city);
 	
 }
