9593f6caca8b66dfee692409832ce509bf6dfaaf
anki009 <ankum1999@gmail.com>
2024-02-19 18:58:27 +0530

    fixed modal popup issue

--- frontend/src/components/Cart/CartModal.jsx
+++ frontend/src/components/Cart/CartModal.jsx
@@ -1,14 +1,20 @@
 /* eslint-disable react/prop-types */
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import Cart from "./Cart";
 import { CartContext } from "../../contexts/CartContext";
 
 function CartModal() {
   const { showModal } = useContext(CartContext);
+  useEffect(
+    () => {
+      
+    },
+    [showModal] 
+  );
 
   return (
     showModal && (
-      <div className="flex-col flex items-center fixed inset-0 left-1/1 sm:left-2/4 md:left-2/3 lg:left-3/4 bg-white dark:bg-black gap-8  p-10  text-black dark:text-white font-normal uppercase text-sm overflow-scroll">
+      <div className=" z-10 flex-col flex items-center fixed inset-0 left-1/1 sm:left-2/4 md:left-2/3 lg:left-3/4 bg-white dark:bg-black gap-8  p-10  text-black dark:text-white font-normal uppercase text-sm overflow-scroll">
         <Cart />
       </div>
     )
