146d4ad5e1e9032b687aae8b0fb6ed4d442a77e7
Nishant Nikam <nikamn@gmx.com>
2024-02-20 06:34:03 +0530

    Added new products2.sql file

---
+++ docs/db/sql_commands/products2.sql
@@ -0,0 +1,11 @@
+INSERT INTO products (id, product_name, price, image_url, quantity, description, category_id) 
+VALUES 
+(1, 'Ammonium Nitrate', 10.99, '/assets/Images/fertilizers/ammoniumNitrate.jpg', 50, 'Product 1 description', 1),
+(2, 'Inhibitor', 19.99, '/assets/Images/fertilizers/inhibitor.jpg', 100, 'Product 2 description', 2),
+(3, 'Nitrogen', 5.99, '/assets/Images/fertilizers/nitrogen.jpg', 20, 'Product 3 description', 1),
+(4, 'Peat', 14.99, '/assets/Images/fertilizers/peat.jpg', 30, 'Product 4 description', 2),
+(5, 'Phoshporous', 8.99, '/assets/Images/fertilizers/phoshporous.jpg', 15, 'Product 5 description', 5),
+(6, 'Potassium', 12.99, '/assets/Images/fertilizers/potassium.jpg', 25, 'Product 6 description', 3),
+(7, 'Slow Release', 9.99, '/assets/Images/fertilizers/slow_release.jpg', 10, 'Product 7 description', 6),
+(8, 'Urea', 17.99, '/assets/Images/fertilizers/urea.jpg', 35, 'Product 8 description', 7),
+(9, 'Vermicompost', 6.99, '/assets/Images/fertilizers/vermicompost.jpg', 18, 'Product 9 description', 3);
